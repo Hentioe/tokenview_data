2566f0486dc291bf945aec94c4fb64bc6593817a
Hentioe <meow.i5.br@gmail.com>
2021-11-30 00:50:21 +0800

    Add data examples

---
+++ examples/hook_receives.exs
@@ -0,0 +1,22 @@
+%{
+  "address" => "TGvw5zXcnzAvphscXeJ6R2de95V72rVSjt",
+  "coin" => "TRX",
+  "confirmations" => 1,
+  "height" => 35_885_819,
+  "time" => 1_638_186_399,
+  "tokenAddress" => "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+  "tokenSymbol" => "USDT",
+  "tokenValue" => "-1080",
+  "txid" => "74d235f7968c506fb600f82742708876b510ecf441eb51b539836c904f2a1666",
+  "value" => "0"
+}
+
+%{
+  "address" => "TGvw5zXcnzAvphscXeJ6R2de95V72rVSjt",
+  "coin" => "TRX",
+  "confirmations" => 1,
+  "height" => 35_886_060,
+  "time" => 1_638_187_122,
+  "txid" => "9d9c16342cdbb3bbc4918949e61ab893eeef6421e2d7a84d1a09c860d3d19769",
+  "value" => "-999.269"
+}
