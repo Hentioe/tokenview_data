d3e82970dfb1b27d1f027b9a3dd57bd8a85c4bb4
Hentioe <meow.i5.br@gmail.com>
2021-11-29 14:06:46 +0800

    Add config module

--- lib/config.ex
+++ lib/config.ex
@@ -1,3 +1,18 @@
 defmodule TokenviewData.Config do
   @moduledoc false
+
+  @spec key :: String.t()
+  def key() do
+    get(:key)
+  end
+
+  @spec builtin_webhook :: boolean()
+  def builtin_webhook() do
+    get(:key)
+  end
+
+  @spec get(atom) :: any
+  def get(key) do
+    Application.get_env(:tokenview_data, key)
+  end
 end
